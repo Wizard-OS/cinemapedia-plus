832017fcbc99503eb5a114285bae94f7e23d5f55
Osvaldo Cintra García <ocintra@tolber.io>
2025-01-31 00:20:05 -0300

    Add horizontal movie list view with poster, title, and rating display

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -20,7 +21,81 @@ class MovieHorizontalListview extends StatelessWidget {
       height: 350,
       child: Column(
         children: [
-          if (title != null || subtitle != null) _Title(title, subtitle)
+          if (title != null || subtitle != null) _Title(title, subtitle),
+          Expanded(
+              child: ListView.builder(
+            itemCount: movies.length,
+            scrollDirection: Axis.horizontal,
+            physics: BouncingScrollPhysics(),
+            itemBuilder: (context, index) {
+              return _Slide(movie: movies[index]);
+            },
+          ))
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+
+  const _Slide({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textStyles = Theme.of(context).textTheme;
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //* Poster
+          SizedBox(
+            width: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                fit: BoxFit.cover,
+                movie.posterPath,
+                width: 150,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return Padding(
+                      padding: EdgeInsets.all(0.8),
+                      child: CircularProgressIndicator(strokeWidth: 2),
+                    );
+                  }
+                  return FadeInRight(child: child);
+                },
+              ),
+            ),
+          ),
+
+          SizedBox(height: 5),
+
+          //* Title
+          SizedBox(
+            width: 150,
+            child: Text(
+              movie.title,
+              maxLines: 2,
+              style: textStyles.titleSmall,
+            ),
+          ),
+
+          //* Rating
+          Row(
+            children: [
+              Icon(Icons.star_half_outlined, size: 15, color: Colors.yellow.shade800),
+              SizedBox(width: 5),
+              Text('${movie.voteAverage}', style: textStyles.bodyMedium?.copyWith(color: Colors.yellow.shade800)),
+              SizedBox(width: 10),
+              Text('(${movie.popularity})', style: textStyles.bodySmall),
+            ],
+          ),
         ],
       ),
     );
