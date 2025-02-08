71b961eef3fd74c748b593272185f5cb4ae47148
Osvaldo Cintra García <ocintra@tolber.io>
2025-02-08 00:21:48 -0300

    Load actors by movie in MovieScreen and update credits response model

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/presentation/providers/actors/actors_by_movie_provider.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
@@ -22,6 +23,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     super.initState();
 
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+    ref.read(actorsByMovieProvider.notifier).loadActors(widget.movieId);
   }
 
   @override
