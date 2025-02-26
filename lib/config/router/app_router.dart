b86cfdca7716dcd782c803d95d50ed1f8fb72f6e
Osvaldo Cintra Garcia <wizardos2011@gmail.com>
2025-02-25 23:55:50 -0300

    Update routing for Home and Movie screens to include page index in navigation

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -5,24 +5,29 @@ final appRouter = GoRouter(
   initialLocation: '/home/0',
   routes: [
     GoRoute(
-        path: '/home/:page',
-        name: HomeScreen.name,
-        builder: (context, state) {
-          final pageIndex = int.parse(state.pathParameters['page'] ?? '0');
+      path: '/home/:page',
+      name: HomeScreen.name,
+      builder: (context, state) {
+        final pageIndex = int.parse(state.pathParameters['page'] ?? '0');
 
-          return HomeScreen(
-            pageIndex: pageIndex,
-          );
-        },
-        routes: [
-          GoRoute(
-            path: 'movie/:id',
-            name: MovieScreen.name,
-            builder: (context, state) {
-              final movieId = state.pathParameters['id'] ?? 'no-id';
-              return MovieScreen(movieId: movieId);
-            },
-          ),
-        ]),
+        return HomeScreen(
+          pageIndex: pageIndex,
+        );
+      },
+      routes: [
+        GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) {
+            final movieId = state.pathParameters['id'] ?? 'no-id';
+            return MovieScreen(movieId: movieId);
+          },
+        ),
+      ],
+    ),
+    GoRoute(
+      path: '/',
+      redirect: (_, __) => '/home/0',
+    ),
   ],
 );
